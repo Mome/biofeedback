13c5623bdded314c46dbdc0a64d4d0fb33171d6d
Moritz <npsjua@gmail.com>
2014-08-22 08:42:18 +0200

    ready for two plots

--- arduino/plot_data.py
+++ arduino/plot_data.py
@@ -1,7 +1,15 @@
 from pylab import * 
 
 data = open('ecg_record').readlines()
-plot(data)
+data = data[3860:]
+data = [d.split(' ') for d in data]
+
+ecg = [d[0] for d in data]
+gsr = [d[1] for d in data]
+
+plot(ecg)
+figure()
+plot(gsr)
 title('Galvanic Skin Response')
 xlabel(r'Milisekunden ($msec$)')
 ylabel(r'Skin Conductance ($\mu S$)')
