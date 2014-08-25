896593e474a4b49d5669af4f00ab3fc905c64707
Raspberry Pi <pi@raspberrypi.(none)>
2014-08-25 21:46:31 +0000

    wrote makefile and reorderd libraries

--- raspi/eHealth_2.3/compile.sh
+++
@@ -1,5 +0,0 @@
-g++ -c arduPi/arduPi.cpp -o arduPi.o
-
-g++ -c eHealth2-raspberry-V2.3/eHealth.cpp -o eHealth.o
-
-g++ -lpthread -lrt gsr.cpp arduPi.o eHealth.o -o GSR 
--- raspi/eHealth_2/compile.sh
+++
@@ -1,5 +0,0 @@
-g++ -c arduPi/arduPi.cpp -o arduPi.o
-
-g++ -c eHealth2-raspberry/eHealth.cpp -o eHealth.o
-
-g++ -lpthread -lrt gsr.cpp arduPi.o eHealth.o -o GSR 
