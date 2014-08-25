8f00a48459159be3e5a48fc19a9376e83a865fe5
Moritz <npsjua@gmail.com>
2014-08-25 17:50:52 +0200

    added ardupi

---
+++ raspi/eHealth_v2.2/gsr.cpp
@@ -0,0 +1,25 @@
+#include "eHealth.h"
+
+void loop() { 
+	
+  float conductance = eHealth.getSkinConductance();
+  float resistance = eHealth.getSkinResistance();
+  float conductanceVol = eHealth.getSkinConductanceVoltage();
+
+  printf("Conductance : %f \n", conductance);    
+  printf("Resistance : %f \n", resistance);  
+  printf("Conductance Voltage : %f \n", conductanceVol);       
+
+  printf("\n");
+
+  // wait for a second  
+  delay(1000);   
+}
+
+int main (){
+	//setup();
+	while(1){
+		loop();
+	}
+	return (0);
+}
