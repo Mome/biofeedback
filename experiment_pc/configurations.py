bf7525095f97cc6290013592e967da58da6b6298
Pc Uni <herrpeterpaul@web.de>
2014-10-21 18:20:23 +0200

    Stream to File works on windows

--- experiment_pc/configurations.py
+++ experiment_pc/configurations.py
@@ -1,18 +1,17 @@
 
 data_path = '~/inlusio_data/records'
 metadata_path = '~/inlusio_data/metadata'
+appdata_path = '~/biofeedback'
 data_delimiter = ','
 
 
 # ---------------- RESOLVE PATHS ---------------- #
-import os                                         #
-data_path = os.path.expanduser(data_path)         #
-if not data_path.endswith('/') :                  #
-	data_path += '/'                              #
-metadata_path = os.path.expanduser(metadata_path) #
-if not metadata_path.endswith('/') :              #
-	metadata_path += '/'                          #
-del os                                            #
+import os
+data_path = os.path.expanduser(data_path)
+data_path = os.path.normpath(data_path)
+metadata_path = os.path.expanduser(metadata_path)
+metadata_path = os.path.normpath(metadata_path)
+appdata_path = os.path.expanduser(appdata_path)
+appdata_path = os.path.normpath(appdata_path)
+del os
 # ----------------------------------------------- #
-
-#TODO path management for WINDOWS
--- experiment_pc/stream_manager.py
+++ experiment_pc/stream_manager.py
@@ -1,15 +1,16 @@
-import argparse
-import experiment_manager
 import math
 import os
 import random
-import serial
 import socket
 import subprocess
 import sys
 import threading
 import time
+
+import serial
+
 import configurations as conf
+
 
 class UdpStreamReader:
     
@@ -21,7 +22,8 @@ class UdpStreamReader:
     def read(self):
         data, addr = self.sock.recvfrom(1024)
         return data
-        
+
+
 class SerialStreamReader():
 
     def __init__(self, port, baud=115200):    
@@ -85,13 +87,15 @@ class FileWriter:
 
     def __init__(self, filename, timed=True):
         
-        FileWriter.check_and_create_data_path()
-
-        path = conf.data_path + filename
+        if not os.path.exists(conf.data_path) :
+            os.makedirs(conf.data_path)
+        
+        path = os.path.normpath(conf.data_path + '/' + filename)
+        
         if os.path.exists(path) :
             raise Exception('Datafile ' + path + ' already exists !')
 
-        self.file = open(conf.data_path + filename,'a')
+        self.file = open(path,'a')
         self.filename = filename
         self.timed=timed
         self.set_starting_time()
@@ -142,18 +146,6 @@ class FileWriter:
 
         return subject_id + '_' + str(record_number) +'.' + ending
 
-    @classmethod
-    def check_and_create_data_path(cls):
-        path = [p for p in conf.data_path.split('/') if p!='']
-        test_path = ''
-        for p in path :
-            test_path += '/' + p
-            if os.path.exists(test_path) :
-                if not os.path.isdir(test_path):
-                    raise Exception(test_path + ' already exists, but is a file !!')
-            else :
-                os.mkdir(test_path)
-                print 'mkdir ' + test_path
 
 class TermWriter:
 
@@ -253,91 +245,4 @@ class UdpStreamer :
             yield line
             if(retcode is not None):
                 break
-
-    
-def main():
-    parser = argparse.ArgumentParser(description='Read a stream and print it to some output interface.')
-    parser.add_argument('-i', '--input', choices=['network','serial'])
-    parser.add_argument('-o', '--output', choices=['terminal','graphical','file'])
-    parser.add_argument('-p', '--port')
-    parser.add_argument('-f', '--filename')
-    args = parser.parse_args()
-    
-    if args.input == None :
-        i = raw_input('Choose platform - 1 for Arduino  or  2 for RasPi  : ')
-        args.input = ['serial','network'][int(i)-1]
-    
-    if args.output == None :
-        i = raw_input('Choose output - 1 for Terminal ,  2 for Graphical  or  3 for File : ')
-        args.output = ['terminal','graphical','file'][int(i)-1]
-    
-    if args.port==None and args.input=='serial' :
-        ports = SerialStreamReader.list_serial_ports()
-        print 'Available ports:', ports
-        if len(ports) > 1 :
-            args.port = raw_input('Choose a port: ')
-        elif len(ports) == 1 :
-            args.port = ports[0]
-            print 'chose port', args.port
-        else :
-            print 'No ports found!'
-            exit()
-    
-    if args.port==None and args.input=='network' :
-        args.port = 49152
-        
-    if args.filename==None and args.output == 'file':
-        filename = raw_input('Please enter filename:')
-           
-    if args.input == 'network'  :
-        stream_reader = UdpStreamReader(args.port)
-        
-    if args.input == 'serial' :
-        stream_reader = SerialStreamReader(args.port)
-    
-    #print args
-    #if raw_input('type exit to exit: ') == 'exit' : exit()
-    
-    bsr = BufferedStreamReader(stream_reader)
-    
-    
-    if args.output == 'terminal' :
-        bsr.start()
-        for i in range(100):
-            print bsr.read()
-            time.sleep(1)
-    
-    if args.output == 'graphical' :
-        plotter = AnimatedPlotter('Skin Conductance (GSR)',bsr,1)
-        bsr.start()
-        plotter.start()
-        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-            QtGui.QApplication.instance().exec_()
-        
-    if args.output == 'file' :
-        pass
-    
-    bsr.stop()
-    print 'Regular termination !'
-    
-
-if __name__=='__main__':
-    reader = DummyStreamReader()
-    
-    #t_writer = TermWriter()
-    #f_writer = FileWriter('pusemuckel')
-    buffered_pipe0  = BufferedPipe()
-    buffered_pipe1  = BufferedPipe()
-
-    manager = StreamManager(reader)
-    #manager.addWriter(t_writer)
-    #manager.addWriter(f_writer)
-    manager.addWriter(buffered_pipe0)
-    manager.addWriter(buffered_pipe1)
-    manager.start()
-    experiment_manager.display_plotter_only(buffered_pipe0,0)
-    experiment_manager.display_plotter_only(buffered_pipe1,1)    
-
-    manager.stop()
-    print 'zuende'
-
+ 
