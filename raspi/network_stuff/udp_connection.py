e651332985f52200a651d1e2ce0217f242c36cb6
Raspberry Pi <pi@inlusiopi.(none)>
2014-11-25 13:55:29 +0000

    added , seperator to udp_connection

--- raspi/network_stuff/udp_connection.py
+++ raspi/network_stuff/udp_connection.py
@@ -14,20 +14,23 @@ def runProcess(exe):
             break
 
 def server(app):
-    ip = '10.42.0.1'
+    #ip = '10.42.0.1' # ad_hoc
+    ip = '131.173.37.185' # windows pc (carpo)
+    #ip = '131.173.37.182' # shitty pc (dolly10)
+    #ip = '131.173.36.240' 3 dhcp ip for moses
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+    
     for line in runProcess(app):
+        #print line.strip()
+	line = line.strip().replace(' ',',')
         sock.sendto(line.strip(), (ip, port))
 
 def client():
-    ip = '10.42.0.2'
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('',port))
     while 1:
         data, addr = sock.recvfrom(1024)
         print "received message:", data
-
 
 
 if __name__=='__main__':
